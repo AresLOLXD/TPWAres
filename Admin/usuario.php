5e3cb6b8d734351e90d7677af7ce32c697ce0a37
iappsan <iann.ipn9@gmail.com>
2021-01-26 22:07:27 -0600

    Agregue bootstrap e hice las paginas de Admin

---
+++ Admin/usuario.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+  <base href="/TPW/">
+  <meta charset="UTF-8">
+  <title>Admin | Registra & edita</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <script src="./js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Proyecto de TPW - - - Administrador</a>
+      <button type="button" class="btn btn-secondary">Cerrar sesión</button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Admin/Usuarios.php">Lista de usuarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Admin/Usuario.php">Registrar usuario</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <!-- Registrar o editar usuario -->
+    <br>
+    <h1 class="display-5">Registra / edita usuario</h1>
+    <br>
+    <form>
+      <div class="container">
+        <div class="row g-2">
+          <div class="col-6">
+            <label for="txtNombre" class="form-label">Nombre:</label>
+            <input type="text" class="form-control" id="txtNombre">
+          </div>
+          <div class="col-6">
+            <label for="txtApPat" class="form-label">Apellido Paterno:</label>
+            <input type="text" class="form-control" id="txtApPat">
+          </div>
+          <div class="col-6">
+            <label for="txtApMat" class="form-label">Apellido Materno:</label>
+            <input type="text" class="form-control" id="txtApMat">
+          </div>
+          <div class="col-6">
+            <label for="txtTipo" class="form-label">Nivel de usuario:</label>
+            <select class="form-select">
+              <option selected>Selecciona una opción ...</option>
+              <option value="0">Alumno</option>
+              <option value="1">Profe</option>
+              <option value="2">Admin</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="mb-3"></div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+</body>
+
+</html>
--- Admin/usuarios.php
+++ Admin/usuarios.php
@@ -1,15 +1,73 @@
-<?php require dirname(dirname(__FILE__)) . "/Util/verifyAdmin.php";?>
 <!DOCTYPE html>
 
 <html lang="es">
-    <head>
-        <base href="/TPW/">
-        <meta charset="UTF-8">
-        <title>Admin</title>
-        <link rel="stylesheet" href="css/bulma.min.css">
-    </head>
-    <body>
-        <script src="js/fontawesome.js"></script>
-        <script src="js/jquery-3.5.1.min.js"></script>
-    </body>
+
+<head>
+  <base href="/TPW/">
+  <meta charset="UTF-8">
+  <title>Admin | Busca & lista</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <script src="./js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Proyecto de TPW - - - Administrador</a>
+      <button type="button" class="btn btn-secondary">Cerrar sesión</button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Admin/Usuarios.php">Lista de usuarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Admin/Usuario.php">Registrar usuario</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-md">
+    <!-- Busqueda de usuarios -->
+    <br>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Nombre de usuario" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Buscar usuario</button>
+    </form>
+  </div>
+
+  <div class="container">
+    <br>
+    <!-- Listado de usuarios -->
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Usuario</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Ap. Paterno</th>
+          <th scope="col">Ap. Materno</th>
+          <th scope="col">Tipo de usuario</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+</body>
+
 </html>
